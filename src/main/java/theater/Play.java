3073cdbd572fa522891274bcb8fa4897368aaf5c
nnoila <naila.anindya.rusdi@gmail.com>
2025-11-18 21:53:02 -0500

    Fixed CheckStyle issues for Play

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,26 @@
 package theater;
 
+/**
+ * Represents a play in the theatre invoicing system.
+ *
+ * @null This class does not allow null values for name or type.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
